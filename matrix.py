98efcb7bb93b60d553bd8764eafc78c6c6ff56de
Intipy <jios6790@gmail.com>
2021-08-21 15:33:12 +0900

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -30,32 +30,46 @@ class MatrixExamples(Scene):
 
 
 
-        g = Group(mat1).arrange_in_grid(buff=2)
-        self.add(g)
+        g = Group(mat1).arrange_in_grid(buff=2)   # 행렬 그리기
+        self.add(g) 
         self.wait(7)
 
 
-        self.play(Write(surr_row_1), run_time=3)
+
+        self.play(Write(surr_row_1), run_time=3)   # 행 박스 그리기
         self.wait(10)
         self.play(ReplacementTransform(surr_row_1,surr_row_2), run_time=2)
         self.wait(7)
 
 
-        self.remove(surr_row_1, surr_row_2)
 
 
-        self.wait(7)     
+        self.remove(surr_row_1, surr_row_2)   # 행 박스 지우기
+
+
+
+
+        self.wait(7)   # 열 박스 그리기
         self.play(Write(surr_col_1), run_time=3)
         self.wait(10)
         self.play(ReplacementTransform(surr_col_1,surr_col_2), run_time=2)
         self.wait(7)
         self.play(ReplacementTransform(surr_col_2,surr_col_3), run_time=2)
         self.wait(7)
-        
 
-        self.remove(surr_col_1, surr_col_2, surr_col_3)
 
         
+
+        self.remove(surr_col_1, surr_col_2, surr_col_3)   # 열 박스 지우기
+
+
+
+
+        surr_col_1 = SurroundingRectangle(mat1.get_columns()[0])   # 행, 열 박스 그리
+        surr_col_2 = SurroundingRectangle(mat1.get_columns()[1])
+        surr_col_3 = SurroundingRectangle(mat1.get_columns()[2])
+        surr_row_1 = SurroundingRectangle(mat1.get_rows()[0])
+        surr_row_2 = SurroundingRectangle(mat1.get_rows()[1])
         self.wait(7)
         self.play(Write(surr_row_1), run_time=3)
         self.play(Write(surr_col_1), run_time=3)
